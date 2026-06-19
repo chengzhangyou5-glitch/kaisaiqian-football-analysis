b619fba11493a17a1d0fe80b76df911295567696
chengzhangyou5-glitch <290524199+chengzhangyou5-glitch@users.noreply.github.com>
2026-06-19 16:28:37 +0800

    Bust cached site data modules

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-import { upcomingPage, detailPage, historyDetailPage, historyPage, searchPage, noticeModal } from "./components.js";
+import { upcomingPage, detailPage, historyDetailPage, historyPage, searchPage, noticeModal } from "./components.js?v=20260619-1384fe7";
 
 const app = document.querySelector("#app");
 const modalLayer = document.querySelector("#modal-layer");
